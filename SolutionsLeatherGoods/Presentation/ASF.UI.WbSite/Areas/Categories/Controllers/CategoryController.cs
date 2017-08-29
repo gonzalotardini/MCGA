7869759a3662691a2fb883f79a6e296c339b7ad5
gonzalotardini <gonzalotardini@gmail.com>
2017-08-29 13:41:00 -0300

    Se logro conectar a la base de datos y ya se muestra lista de categorias

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Categories/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Areas.Categories.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Categories/Category
+        public ActionResult Index()
+        {
+            var CategoryProcess = new Process.CategoryProcess();
+            var Lista = CategoryProcess.SelectList();
+            return View(Lista);
+        }
+    }
+}
