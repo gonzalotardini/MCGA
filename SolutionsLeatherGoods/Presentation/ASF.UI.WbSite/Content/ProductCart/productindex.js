c5caaf3fdcdc30849bc40aea0395a447aae723fd
gonzalotardini <gonzalotardini@gmail.com>
2017-10-30 14:00:37 -0300

    intento de creacion de add to cart

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Content/ProductCart/productindex.js
@@ -0,0 +1,20 @@
+﻿
+$("#addit").click(function () {
+
+    alert("You entered p1!");
+        //var productId = $("#productId").val();
+        //var productName = $("#productName").val();
+        //var productQuantity = $("#productQuantity").val();
+        //var data = {
+        //'product_id': productId,
+        //    'product_name': productName,
+        //    'quantity': productQuantity
+        //};
+
+        //$.post("/cart/add", data, showDone);
+    });
+
+    var showDone = function () {
+        
+    }
+
--- SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Controllers/ProductController.cs
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Controllers/ProductController.cs
@@ -123,6 +123,11 @@ namespace ASF.UI.WbSite.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public ActionResult addTocart()
+        {
+            return View();
+        }
     }
 
 }
